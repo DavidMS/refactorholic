1734445c5fdc6e63adf71f1636fa673f64393163
Tomas Bozzo <tomasbozzo@gmail.com>
2020-09-20 16:48:21 +0200

    Add /posts/:slug page

--- hosting/blog-app/src/App.js
+++ hosting/blog-app/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import PostPage from "./pages/PostPage";
+import {ApolloProvider} from "@apollo/client";
+import client from "./graphql/client";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ApolloProvider client={client}>
+          <BrowserRouter>
+              <Switch>
+                  <Route exact path="/posts/:slug" component={PostPage}/>
+              </Switch>
+          </BrowserRouter>
+      </ApolloProvider>
   );
 }
 
